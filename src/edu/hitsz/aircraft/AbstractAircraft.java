aef6cc241006c437678bba9450610257a536d3b8
py c <uchitachipy@gmail.com>
2026-03-25 13:30:25 +0800

    initial-updata

---
+++ src/edu/hitsz/aircraft/AbstractAircraft.java
@@ -0,0 +1,46 @@
+package edu.hitsz.aircraft;
+
+import edu.hitsz.bullet.BaseBullet;
+import edu.hitsz.basic.AbstractFlyingObject;
+import java.util.List;
+
+/**
+ * 所有种类飞机的抽象父类
+ * @author hitsz
+ */
+public abstract class AbstractAircraft extends AbstractFlyingObject {
+
+    //最大生命值
+    protected int maxHp;
+    protected int hp;
+
+    public AbstractAircraft(int locationX, int locationY, int speedX, int speedY, int hp) {
+        super(locationX, locationY, speedX, speedY);
+        this.hp = hp;
+        this.maxHp = hp;
+    }
+
+    public void decreaseHp(int decrease){
+        hp -= decrease;
+        if(hp <= 0){
+            hp=0;
+            vanish();
+        }
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+
+    /**
+     * 飞机射击方法
+     * @return
+     *  可射击对象需实现，返回子弹列表
+     *  非可射击对象空实现，返回空列表
+     */
+    public abstract List<BaseBullet> shoot();
+
+}
+
+
---
+++ src/edu/hitsz/application/Game.java
@@ -0,0 +1,268 @@
+package edu.hitsz.application;
+
+import edu.hitsz.aircraft.*;
+import edu.hitsz.bullet.BaseBullet;
+import edu.hitsz.basic.AbstractFlyingObject;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.*;
+import java.util.List;
+import java.util.Timer;
+import java.util.concurrent.*;
+
+/**
+ * 游戏主面板，游戏启动
+ * @author hitsz
+ */
+public class Game extends JPanel {
+
+    private int backGroundTop = 0;
+
+    //调度器, 用于定时任务调度
+    private final Timer timer;
+    //时间间隔(ms)，控制刷新频率
+    private final int timeInterval = 40;
+
+    private final HeroAircraft heroAircraft;
+    private final List<AbstractAircraft> enemyAircrafts;
+    private final List<BaseBullet> heroBullets;
+    private final List<BaseBullet> enemyBullets;
+
+    //屏幕中出现的敌机最大数量
+    private final int enemyMaxNumber = 5;
+
+    //敌机生成周期
+    protected double enemySpawnCycle  =  20;
+    private int enemySpawnCounter = 0;
+
+    //英雄机和敌机射击周期
+    protected double shootCycle = 20;
+    private int shootCounter = 0;
+
+    //当前玩家分数
+    private int score = 0;
+
+    //游戏结束标志
+    private boolean gameOverFlag = false;
+
+    public Game() {
+        heroAircraft = new HeroAircraft(
+                Main.WINDOW_WIDTH / 2,
+                Main.WINDOW_HEIGHT - ImageManager.HERO_IMAGE.getHeight() ,
+                0, 0, 100);
+
+        enemyAircrafts = new LinkedList<>();
+        heroBullets = new LinkedList<>();
+        enemyBullets = new LinkedList<>();
+
+        //启动英雄机鼠标监听
+        new HeroController(this, heroAircraft);
+
+        this.timer = new Timer("game-action-timer", true);
+
+    }
+
+    /**
+     * 游戏启动入口，执行游戏逻辑
+     */
+    public void action() {
+
+        // 定时任务：绘制、对象产生、碰撞判定、及结束判定
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+
+                enemySpawnCounter++;
+                if (enemySpawnCounter >=enemySpawnCycle) {
+                    enemySpawnCounter = 0;
+                    // 产生普通敌机
+                    if (enemyAircrafts.size() < enemyMaxNumber) {
+                        enemyAircrafts.add(new MobEnemy(
+                                (int) (Math.random() * (Main.WINDOW_WIDTH - ImageManager.MOB_ENEMY_IMAGE.getWidth())),
+                                (int) (Math.random() * Main.WINDOW_HEIGHT * 0.05),
+                                0,
+                                10,
+                                30
+                        ));
+                    }
+                }
+
+                // 飞机发射子弹
+                shootAction();
+                // 子弹移动
+                bulletsMoveAction();
+                // 飞机移动
+                aircraftsMoveAction();
+                // 撞击检测
+                crashCheckAction();
+                // 后处理
+                postProcessAction();
+                // 重绘界面
+                repaint();
+                // 游戏结束检查
+                checkResultAction();
+            }
+        };
+        // 以固定延迟时间进行执行：本次任务执行完成后，延迟 timeInterval 再执行下一次
+        timer.schedule(task,0,timeInterval);
+
+    }
+
+    //***********************
+    //      Action 各部分
+    //***********************
+
+    private void shootAction() {
+        shootCounter++;
+        if (shootCounter >= shootCycle) {
+            shootCounter = 0;
+            //英雄机射击
+            heroBullets.addAll(heroAircraft.shoot());
+            // TODO 敌机射击
+        }
+    }
+
+    private void bulletsMoveAction() {
+        for (BaseBullet bullet : heroBullets) {
+            bullet.forward();
+        }
+        for (BaseBullet bullet : enemyBullets) {
+            bullet.forward();
+        }
+    }
+
+    private void aircraftsMoveAction() {
+        for (AbstractAircraft enemyAircraft : enemyAircrafts) {
+            enemyAircraft.forward();
+        }
+    }
+
+
+    /**
+     * 碰撞检测：
+     * 1. 敌机攻击英雄
+     * 2. 英雄攻击/撞击敌机
+     * 3. 英雄获得补给
+     */
+    private void crashCheckAction() {
+        // TODO 敌机子弹攻击英雄机
+
+        // 英雄子弹攻击敌机
+        for (BaseBullet bullet : heroBullets) {
+            if (bullet.notValid()) {
+                continue;
+            }
+            for (AbstractAircraft enemyAircraft : enemyAircrafts) {
+                if (enemyAircraft.notValid()) {
+                    // 已被其他子弹击毁的敌机，不再检测
+                    // 避免多个子弹重复击毁同一敌机的判定
+                    continue;
+                }
+                if (enemyAircraft.crash(bullet)) {
+                    // 敌机撞击到英雄机子弹
+                    // 敌机损失一定生命值
+                    enemyAircraft.decreaseHp(bullet.getPower());
+                    bullet.vanish();
+                    if (enemyAircraft.notValid()) {
+                        // TODO 获得分数，产生道具补给
+                        score += 10;
+                    }
+                }
+                // 英雄机 与 敌机 相撞，均损毁
+                if (enemyAircraft.crash(heroAircraft) || heroAircraft.crash(enemyAircraft)) {
+                    enemyAircraft.vanish();
+                    heroAircraft.decreaseHp(Integer.MAX_VALUE);
+                }
+            }
+        }
+
+        // Todo: 我方获得道具，道具生效
+
+    }
+
+    /**
+     * 后处理：
+     * 1. 删除无效的子弹
+     * 2. 删除无效的敌机
+     * 3. 删除无效的道具
+     */
+    private void postProcessAction() {
+        enemyBullets.removeIf(AbstractFlyingObject::notValid);
+        heroBullets.removeIf(AbstractFlyingObject::notValid);
+        enemyAircrafts.removeIf(AbstractFlyingObject::notValid);
+        // Todo: 删除无效道具
+    }
+
+    /**
+     * 检查游戏是否结束，若结束：关闭线程池
+     */
+    private void checkResultAction(){
+        // 游戏结束检查英雄机是否存活
+        if (heroAircraft.getHp() <= 0) {
+            timer.cancel(); // 取消定时器并终止所有调度任务
+            gameOverFlag = true;
+            System.out.println("Game Over!");
+        }
+    };
+
+    //***********************
+    //      Paint 各部分
+    //***********************
+    /**
+     * 重写 paint方法
+     * 通过重复调用paint方法，实现游戏动画
+     */
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+
+        // 绘制背景,图片滚动
+        g.drawImage(ImageManager.BACKGROUND_IMAGE, 0, this.backGroundTop - Main.WINDOW_HEIGHT, null);
+        g.drawImage(ImageManager.BACKGROUND_IMAGE, 0, this.backGroundTop, null);
+        this.backGroundTop += 1;
+        if (this.backGroundTop == Main.WINDOW_HEIGHT) {
+            this.backGroundTop = 0;
+        }
+
+        // 先绘制子弹，后绘制飞机
+        // 这样子弹显示在飞机的下层
+        paintImageWithPositionRevised(g, enemyBullets);
+        paintImageWithPositionRevised(g, heroBullets);
+        paintImageWithPositionRevised(g, enemyAircrafts);
+
+        // Todo: 绘制道具
+
+        g.drawImage(ImageManager.HERO_IMAGE, heroAircraft.getLocationX() - ImageManager.HERO_IMAGE.getWidth() / 2,
+                heroAircraft.getLocationY() - ImageManager.HERO_IMAGE.getHeight() / 2, null);
+
+        //绘制得分和生命值
+        paintScoreAndLife(g);
+
+    }
+
+    private void paintImageWithPositionRevised(Graphics g, List<? extends AbstractFlyingObject> objects) {
+        if (objects.isEmpty()) {
+            return;
+        }
+
+        for (AbstractFlyingObject object : objects) {
+            BufferedImage image = object.getImage();
+            assert image != null : objects.getClass().getName() + " has no image! ";
+            g.drawImage(image, object.getLocationX() - image.getWidth() / 2,
+                    object.getLocationY() - image.getHeight() / 2, null);
+        }
+    }
+
+    private void paintScoreAndLife(Graphics g) {
+        int x = 10;
+        int y = 25;
+        g.setColor(Color.RED);
+        g.setFont(new Font("SansSerif", Font.BOLD, 22));
+        g.drawString("SCORE: " + this.score, x, y);
+        y = y + 20;
+        g.drawString("LIFE: " + this.heroAircraft.getHp(), x, y);
+    }
+
+}
